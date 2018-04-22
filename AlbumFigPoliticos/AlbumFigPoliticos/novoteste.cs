2874e01037666f50f0666b0f05a2a42575ee46c7
CarlosAReis <38595895+CarlosAReis@users.noreply.github.com>
2018-04-21 22:11:25 -0300

    testando denovo
    novo teste adicionada

---
+++ AlbumFigPoliticos/AlbumFigPoliticos/novoteste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumFigPoliticos
+{
+    class novoteste
+    {
+        private string testandodenovo;
+
+        public string Testandodenovo { get => testandodenovo; set => testandodenovo = value; }
+    }
+}
