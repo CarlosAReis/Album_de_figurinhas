a423c13a96d7093ac8e940bfbda31c046420b9ff
CarlosAReis <38595895+CarlosAReis@users.noreply.github.com>
2018-04-21 22:03:35 -0300

    ok
    ok

---
+++ AlbumFigPoliticos/AlbumFigPoliticos/FigurePolitico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumFigPoliticos
+{
+    class FigurePolitico:Figurinha
+    {
+        private string partido;
+        private int chapa;
+                    //Deputado Federal: 4 dígitos + Confirma.
+                    //Seguido por Senador: 3 dígitos + Confirma.
+                    //Governador: 2 dígitos + Confirma
+                    //Presidente: 2 dígitos + Confirma
+        private string cargo; 
+    }
+}
---
+++ AlbumFigPoliticos/AlbumFigPoliticos/FrPacoteFig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlbumFigPoliticos
+{
+    public partial class FrPacoteFig : Form
+    {
+        public FrPacoteFig()
+        {
+            InitializeComponent();
+            MessageBox.Show("Parabens, você ganhou o pacote de figurinhas do dia!");
+        }
+
+        private int Randomico()
+        {
+            Random gerador = new Random();
+            int num;
+
+            num = gerador.Next(1, 6);
+
+            return num;
+        }
+        private void FrPacoteFig_Load(object sender, EventArgs e)
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString() ;
+            try
+            {
+                string nome1, nome2, nome3, nome4, pathaux;
+                nome1 = "fig" + Randomico();
+                Thread.Sleep(10);
+                nome2 = "fig" + Randomico();
+                Thread.Sleep(10);
+                nome3 = "fig" + Randomico();
+                Thread.Sleep(10);
+                nome4 = "fig" + Randomico();
+               
+                pathaux = path + nome1 + ".png";
+                Image img = Image.FromFile(pathaux);
+                picBox1.Image = img;
+                pathaux = path + nome2 + ".png";
+                img = Image.FromFile(pathaux);
+                picBox2.Image = img;
+                pathaux = path + nome3 + ".png";
+                img = Image.FromFile(pathaux);
+                picBox3.Image = img;
+                pathaux = path + nome4 + ".png";
+                img = Image.FromFile(pathaux);
+                picBox4.Image = img;
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+
+        }
+    }
+}
